8d2c50e7acc3255bcab2e77c29f6491a6e29b6a4
Miguel Silva <miguelsilva5989@gmail.com>
2024-01-01 22:16:03 +0100

    repl init

---
+++ README.md
@@ -0,0 +1,9 @@
+# FPS Lang
+
+A "frames per second" meme language to learn how to develop a compiler.
+
+## Example
+
+```
+#a:=1;#
+```
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, Write, stdin};
+
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Cli {
+    /// To access the REPL
+    #[arg(short, long)]
+    repl: bool,
+
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    if !args.repl {
+        let _ = include_str!("sample.fps");
+    } else {
+        println!("REPL for FPS Lang");
+        println!("-----------------");
+        println!("Type '\\q' or press 'Ctrl+Z' to exit");
+        let mut quit = false;
+        let mut statement = String::new();
+        while !quit {
+            statement.clear();
+            print!("fps> ");
+            stdout().flush().unwrap();
+            if let 0 = stdin().read_line(&mut statement).unwrap() {
+                return;
+            };
+            statement = statement.as_str().trim_end().to_string();
+            if statement == "\\q" {
+                quit = true;
+            }
+        }
+    }
+    
+}
